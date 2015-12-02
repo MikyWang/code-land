ae2379be39c79108c0993a526196eb9d196d77ff
王齐源 <wqy813853090@live.cn>
2015-12-02 17:49:17 +0800

    add a Multithreaded version.

--- GTA5Net/GTA5Net/View/Net5.xaml.cs
+++ GTA5Net/GTA5Net/View/Net5.xaml.cs
@@ -27,7 +27,8 @@ namespace GTA5Net.View
     {
         private NotifyType _notifyType;
         public NetViewModel NetViewModel { get; set; }
-        public static int Count =0;
+        public static int Count = 0;
+        private WebViewType _webViewType;
         public Net5()
         {
             NetViewModel = new NetViewModel();
@@ -36,13 +37,74 @@ namespace GTA5Net.View
         }
         private async void NetWeb_DOMContentLoaded(WebView sender, WebViewDOMContentLoadedEventArgs args)
         {
-            await NetWeb.InvokeScriptAsync("eval", new string[] { IPSource.IPHelper.GetDomain() });
-            NetWeb.DOMContentLoaded -= NetWeb_DOMContentLoaded;
-            await Task.Delay(2000);
-            await NetWeb.InvokeScriptAsync("eval", new string[] { IPSource.IPHelper.Script3 });
+            sender.DOMContentLoaded -= NetWeb_DOMContentLoaded;
+            _webViewType = (WebViewType)Enum.Parse(typeof(WebViewType), sender.Name);
+            switch (_webViewType)
+            {
+                case WebViewType.NetWeb0:
+                    Count = 0;
+                    break;
+                case WebViewType.NetWeb1:
+                    Count = 1;
+                    break;
+                case WebViewType.NetWeb2:
+                    Count = 2;
+                    break;
+                case WebViewType.NetWeb3:
+                    Count = 3;
+                    break;
+                case WebViewType.NetWeb4:
+                    Count = 4;
+                    break;
+                case WebViewType.NetWeb5:
+                    Count = 5;
+                    break;
+                case WebViewType.NetWeb6:
+                    Count = 6;
+                    break;
+                case WebViewType.NetWeb7:
+                    Count = 7;
+                    break;
+                default:
+                    break;
+            }
+            await sender.InvokeScriptAsync("eval", new string[] { IPSource.IPHelper.GetDomain() });
+            await Task.Delay(5000);
+            await sender.InvokeScriptAsync("eval", new string[] { IPSource.IPHelper.GetProgress() });
         }
         private async void NetWeb_ScriptNotify(object sender, NotifyEventArgs e)
         {
+            var webview = sender as WebView;
+            _webViewType = (WebViewType)Enum.Parse(typeof(WebViewType), webview.Name);
+            switch (_webViewType)
+            {
+                case WebViewType.NetWeb0:
+                    Count = 0;
+                    break;
+                case WebViewType.NetWeb1:
+                    Count = 1;
+                    break;
+                case WebViewType.NetWeb2:
+                    Count = 2;
+                    break;
+                case WebViewType.NetWeb3:
+                    Count = 3;
+                    break;
+                case WebViewType.NetWeb4:
+                    Count = 4;
+                    break;
+                case WebViewType.NetWeb5:
+                    Count = 5;
+                    break;
+                case WebViewType.NetWeb6:
+                    Count = 6;
+                    break;
+                case WebViewType.NetWeb7:
+                    Count = 7;
+                    break;
+                default:
+                    break;
+            }
             var datas = e.Value.Split(',');
             _notifyType = (NotifyType)Enum.Parse(typeof(NotifyType), datas[0]);
             var data = datas[1];
@@ -54,76 +116,77 @@ namespace GTA5Net.View
                         NetViewModel.IpMods[Count].Progress = data;
                         NetViewModel.IpMods[Count].ProgressValue = data.Split('%')[0];
                         await Task.Delay(100);
-                        await NetWeb.InvokeScriptAsync("eval", new string[] { IPSource.IPHelper.Script3 });
+                        await webview.InvokeScriptAsync("eval", new string[] { IPSource.IPHelper.GetProgress() });
                     }
                     if (data == "100%")
                     {
                         NetViewModel.IpMods[Count].Progress = data;
                         NetViewModel.IpMods[Count].ProgressValue = data.Split('%')[0];
                         NetViewModel.IpMods[Count].IsPopUp = false;
-                        await NetWeb.InvokeScriptAsync("eval", new string[] { IPSource.IPHelper.Script2 });
+                        await webview.InvokeScriptAsync("eval", new string[] { IPSource.IPHelper.GetData() });
 
                     }
                     break;
                 case NotifyType.Data:
-                    var ipTTL = data.Split('&');
-                    data = string.Empty;
-                    var i = 0;
-                    while (i < ipTTL.Length - 1)
-                    {
-                        if ((ipTTL[i].Length > 4 && ipTTL[i + 1].Length > 4) || (ipTTL[i].Length <= 4 && ipTTL[i + 1].Length <= 4))
-                        {
-                            data += ipTTL[i];
-                            i += 2;
-                        }
-                        else
-                        {
-                            data += ipTTL[i];
-                            i++;
-                            if (i == ipTTL.Length - 1)
-                            {
-                                data += ":";
-                                data += ipTTL[i];
-                            }
-                        }
-                        if (i <= ipTTL.Length - 2)
-                        {
-                            data += ":";
-                        }
-
-                    }
-                    var list = data.Split(':');
-                    int min = 1;
-                    for (int j = 1; j < list.Length; j = j + 2)
-                    {
-                        if (int.Parse(list[j]) < int.Parse(list[min]))
-                        {
-                            min = j;
-                        }
-                    }
-                    var ipMod = new IPMod
-                    {
-                        IP = list[min - 1],
-                        TTL = list[min]
-                    };
-                    NetViewModel.IpMods[Count].IP = list[min-1];
-                    NetViewModel.IpMods[Count].TTL = list[min];
-                    Count++;
-                    if (Count<8)
-                    {
-                        NetWeb.Navigate(new Uri("http://tool.chinaz.com/dns/"));
-                        NetWeb.DOMContentLoaded += NetWeb_DOMContentLoaded;
-                    }
+                    NetViewModel.IpMods[Count].IP = getData(getDataFilter(data))[0];
+                    NetViewModel.IpMods[Count].TTL = getData(getDataFilter(data))[1];
                     break;
                 default:
                     break;
             }
         }
-
+        private string getDataFilter(string data)
+        {
+            var ipTTL = data.Split('&');
+            data = string.Empty;
+            var i = 0;
+            while (i < ipTTL.Length-1)
+            {
+                data += ipTTL[i];
+                if (i < ipTTL.Length)
+                {
+                    data += ":";
+                }
+                if (ipTTL[i].Length > 4)
+                {
+                    i++;
+                    while (ipTTL[i].Length > 4)
+                    {
+                        i++;
+                    }
+                }
+                else if (ipTTL[i].Length <= 4 && i < ipTTL.Length - 1)
+                {
+                    i++;
+                    while (ipTTL[i].Length <= 4 && i < ipTTL.Length - 1)
+                    {
+                        i++;
+                    }
+                }
+                if (i == ipTTL.Length - 1)
+                {
+                    data += ipTTL[i];
+                    i++;
+                }
+            }
+            return data;
+        }
+        private string[] getData(string data)
+        {
+            var list = data.Split(':');
+            int min = 1;
+            for (int j = 1; j < list.Length; j = j + 2)
+            {
+                if (int.Parse(list[j]) < int.Parse(list[min]))
+                {
+                    min = j;
+                }
+            }
+            return new string[] { list[min-1], list[min] };
+        }
         private void NetWeb_NavigationFailed(object sender, WebViewNavigationFailedEventArgs e)
         {
-            NetWeb.Refresh();
-            
+            (sender as WebView).Refresh();
         }
     }
 }
